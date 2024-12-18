e12a1666ab14600394292c6ec264ccd91da17ebd
Maxence Nho <78363268+PiKwaii93@users.noreply.github.com>
2024-12-18 16:54:02 +0100

    feat(kpis): fix Law Cat / Offense in Range

--- src/controllers/user/kpi.controller.ts
+++ src/controllers/user/kpi.controller.ts
@@ -74,7 +74,6 @@ router.get("/getByRange", async (req: Request, res: Response) => {
 });
 
 
-
 router.get("/getKpisByRange", async (req: Request, res: Response) => {
   try {
     const { startDate, endDate } = req.query;
@@ -107,7 +106,13 @@ router.get("/getKpisByRange", async (req: Request, res: Response) => {
 
     // Liste des catégories pour offenses
     const categories = {
-      drugOffenses: ["CONTROLLED SUBSTANCE, SALE", "DRUG", "NARCOTICS"],
+      drugOffenses: [
+        "CONTROLLED SUBSTANCE, SALE", 
+        "CONTROLLED SUBSTANCE, POSSESSI", 
+        "CONTROLLED SUBSTANCE, INTENT TO",
+        "DRUG", 
+        "NARCOTICS"
+      ],
       propertyOffenses: [
         "LARCENY",
         "BURGLARY",
@@ -118,6 +123,13 @@ router.get("/getKpisByRange", async (req: Request, res: Response) => {
         "UNAUTHORIZED USE VEHICLE",
         "DISORDERLY CONDUCT",
         "TRESPASS",
+        "LARCENY, GRAND BY FALSE PROMISE-NOT IN PERSON CONTACT",
+        "LARCENY, PETIT FROM STORE-SHOPL",
+        "LARCENY, GRAND FROM VEHICLE/MOTORCYCLE",
+        "LARCENY, GRAND BY BANK ACCT COMPROMISE-REPRODUCED CHECK",
+        "LARCENY, PETIT FROM AUTO",
+        "LARCENY, GRAND FROM RESIDENCE/BUILDING, UNATTENDED, PACKAGE THEFT OUTSIDE",
+        "LARCENY, PETIT FROM STORE-SHOPL"
       ],
       personOffenses: [
         "ASSAULT",
@@ -127,8 +139,37 @@ router.get("/getKpisByRange", async (req: Request, res: Response) => {
         "MENACING",
         "OBSTR BREATH/CIRCUL",
         "VIOLATION OF ORDER OF PROTECTI",
+        "ASSAULT 3",
+        "ASSAULT 2,1, UNCLASSIFIED",
+        "AGGRAVATED HARASSMENT 2",
+        "STRANGULATION 1ST",
+        "ASSAULT POLICE/PEACE OFFICER",
+        "AGGRAVATED HARASSMENT 1",
+        "HARASSMENT, SUBD 1, CIVILIAN",
+        "AGGRAVATED HARASSMENT 2",
+        "ASSAULT 2,1, UNCLASSIFIED"
       ],
+      otherOffenses: [
+        "SODOMY 3",
+        "ROBBERY, BEGIN AS SHOPLIFTING",
+        "LEAVING SCENE-ACCIDENT-PERSONA",
+        "TRAFFIC, UNCLASSIFIED MISDEMEAN",
+        "MISCHIEF, CRIMINAL 3 & 2, OF M",
+        "NY STATE LAWS, UNCLASSIFIED FEL",
+        "RECKLESS ENDANGERMENT 1",
+        "IMPERSONATION, UNCLASSIFIED",
+        "VIOLATION OF ORDER OF PROTECTI",
+        "WEAPONS, POSSESSION, ETC",
+        "CHILD, ENDANGERING WELFARE",
+        "MATERIAL OFFENSIV",
+        "RAPE 1",
+        "BURGLARY, COMMERCIAL, NIGHT",
+        "CRIMINAL POSSESSION WEAPON",
+        "MISCHIEF, CRIMINAL, UNCL 2ND"
+      ]
     };
+    
+    
 
     // Variables pour le calcul global des KPIs
     let totalFelony = 0;
@@ -137,6 +178,7 @@ router.get("/getKpisByRange", async (req: Request, res: Response) => {
     let totalDrugOffenses = 0;
     let totalPropertyOffenses = 0;
     let totalPersonOffenses = 0;
+    let totalOtherOffenses = 0;
 
     crimesInRange.forEach((crime) => {
       const lawCategoryLabel = crime.lawCategory?.label?.toUpperCase();
@@ -168,6 +210,9 @@ router.get("/getKpisByRange", async (req: Request, res: Response) => {
               case "personOffenses":
                 totalPersonOffenses += 1;
                 break;
+              case "otherOffenses":
+                totalOtherOffenses += 1;
+                break;
             }
           }
         });
@@ -177,16 +222,22 @@ router.get("/getKpisByRange", async (req: Request, res: Response) => {
     // Calcul du total général
     const totalCrime = crimesInRange.length;
 
+    // Calcul des pourcentages
+    const percentDrugOffenses = totalCrime ? (totalDrugOffenses / totalCrime) * 100 : 0;
+    const percentPropertyOffenses = totalCrime ? (totalPropertyOffenses / totalCrime) * 100 : 0;
+    const percentPersonOffenses = totalCrime ? (totalPersonOffenses / totalCrime) * 100 : 0;
+    const percentOtherOffenses = totalCrime ? (totalOtherOffenses / totalCrime) * 100 : 0;
+
     // Construction de la réponse finale avec KPIs
     const response = {
-      totalCrime,
       kpis: {
-        FELONY: totalFelony,
-        VIOLATION: totalViolation,
-        MISDEMEANOR: totalMisdemeanor,
-        drugOffenses: totalDrugOffenses,
-        propertyOffenses: totalPropertyOffenses,
-        personOffenses: totalPersonOffenses,
+        countFelony: totalFelony,
+        countViolation: totalViolation,
+        countMisdemeanor: totalMisdemeanor,
+        percentDrugOffenses: percentDrugOffenses,
+        percentPropertyOffenses: percentPropertyOffenses,
+        percentPersonOffenses: percentPersonOffenses,
+        percentOtherOffenses: percentOtherOffenses,
       },
     };
 
